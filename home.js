5960c268a9f2b3081fec079f9de399a59a519631
Blake Alvarez <blake.alvarez00@gmail.com>
2022-03-30 13:40:14 -0500

    Added accessibility menu, refactored home html, added dark/light mode toggle

--- home.js
+++ home.js
@@ -16,3 +16,35 @@ function toTheTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+accessBtn = document.getElementById("accessBtn")
+function openAccessMenu() {
+    var div = document.getElementById("accessMenu")
+    div.style.width = "25%"
+    document.getElementById("accessBtn").style.display="none"
+};
+function closeAccessMenu() {
+    var div = document.getElementById("accessMenu")
+    div.style.width = "0";
+    document.getElementById("accessBtn").style.display="block"
+}
+
+// function darkMode() {
+//     var element = document.body;
+//     var e2 = document.getElementsByClassName("section1");
+//     // element.classList.toggle("darkMode");
+//     e2.toggle("darkMode");
+// }
+
+
+window.onload=function(){
+    let btn = document.querySelector("#switch-theme");
+    let sec2 = document.getElementsByClassName('section2');
+    btn.addEventListener("click", function() {
+        let theme = document.querySelectorAll("[theme]");
+        for (let i = 0; i < theme.length; i++) {
+            theme[i].setAttribute('theme', theme[i].getAttribute('theme') === 'dark' ? 'light' : 'dark');
+            
+        }
+    });
+}
