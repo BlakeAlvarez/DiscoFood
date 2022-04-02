b935d15852049118a461a12ec844debdfe5a70dc
Blake Alvarez <blake.alvarez00@gmail.com>
2022-04-01 20:23:25 -0500

    created slideshow on food page

--- food.js
+++ food.js
@@ -42,7 +42,34 @@ for(var i=0; i<len;i++){
 });
 document.getElementById("FoodList").appendChild(div);
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
+document.getElementById("nextSlide").addEventListener("click", function(){
+  showSlides(slideIndex += 1);
+});
+document.getElementById("prevSlide").addEventListener("click", function(){
+  showSlides(slideIndex += -1);
+});
