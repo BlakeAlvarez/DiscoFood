8cd19db814d72ecc264d230e552f3237f48ed92b
David Chacon <davidsilverxiii@gmail.com>
2022-04-11 17:38:35 -0500

    drinks reviews shows top 5

--- drink.js
+++ drink.js
@@ -311,7 +311,7 @@ document.getElementById("prevSlide").addEventListener("click", function(){
   showSlides(slideIndex += -1);
 });
 
-//Lines 78-348 is used to display the first 5 reviews on drink Page
+//Lines 78-348 is used to display the first 5 reviews on Food Page
 let Reviews = document.createElement('div');
 //var UsersTest = document.createElement('p'); //TEST STATEMENT DELETE LATER
 onValue(Menu, (snapshot) => {
@@ -546,10 +546,11 @@ for(let x in data.Reviews){
     ReviewDes.style.cssText="display:block;transform:translateX(70px);margin-top:-20px;";
     Review.appendChild(ReviewDes);
     Reviews.appendChild(Review);
-    }
     MaxReviews++;
     if(MaxReviews == 5)
       break;
+    }
+    
 }
 /*
 // EXAMPLE- How to loop through database regardless of the key is 
